ece1ebb700e1ab2b1a8e5d0c650b4ea1d0870a8f
Timur Shmarakhmanov <t.shamsrakhmanov@gmail.com>
2024-12-16 14:09:53 +0400

    quality-of-life changes

--- python_frontend/project/frontend.py
+++ python_frontend/project/frontend.py
@@ -3,43 +3,54 @@ import requests
 
 backend_host = 'http://localhost:1234'
 
-rows = [{'id': 'none', 'name': 'none'}]
-columns = [{'label': 'id', 'field': 'id'}, {'label': 'name', 'field': 'name'}]
+with ui.row() as column1:
 
-button_save = ui.button(text='Load users', on_click=lambda: update_users_table())
+    with ui.column() as column2:
 
-table = ui.table(columns=columns, rows=rows, )
+        rows = [{'id': 'none', 'name': 'none'}]
+        columns = [{'label': 'id', 'field': 'id'}, {'label': 'name', 'field': 'name'}]
 
-name_input = ui.input('NAME')
-surname_input = ui.input('SURNAME')
-button_add_user = ui.button(text='Add user', on_click=lambda: add_user())
+        button_get_users = ui.button(text='Load users', on_click=lambda: update_users_table())
 
-id_input = ui.input('ID')
-button_delete = ui.button(text='Remove user', on_click=lambda: remove_user())
+        table = ui.table(columns=columns, rows=rows)
+        table.set_visibility(False)
+
+    with ui.column() as column3:
+
+        name_input = ui.input('NAME')
+
+        surname_input = ui.input('SURNAME')
+        button_add_user = ui.button(text='Add user', on_click=lambda: add_user())
+
+        id_input = ui.input('ID')
+        button_delete = ui.button(text='Remove user', on_click=lambda: remove_user())
 
 
 def remove_user():
-    response = requests.delete(f'{backend_host}/delete_user/', params=f'id={id_input.value}')
-    update_users_table()
-
-
-def clear_user_input():
+    requests.delete(f'{backend_host}/delete_user/', params=f'id={id_input.value}')
     id_input.value = ''
     name_input.value = ''
     surname_input.value = ''
-
+    table.set_visibility(False)
 
 def add_user():
-    response = requests.post(f'{backend_host}/add_user/', json={'name': name_input.value, 'surname': surname_input.value})
-    update_users_table()
+    requests.post(f'{backend_host}/add_user/', json={'name': name_input.value, 'surname': surname_input.value})
+    id_input.value = ''
+    name_input.value = ''
+    surname_input.value = ''
+    table.set_visibility(False)
 
 
 def update_users_table():
+    if not table.visible:
+        table.set_visibility(True)
     response = requests.get(f"{backend_host}/get_users")
     response_dict = response.json()
     table.rows = [{'id': str(k), 'name': str(v)} for k, v in response_dict.items()]
+    id_input.value = ''
+    name_input.value = ''
+    surname_input.value = ''
     table.update()
-    clear_user_input()
 
 
 ui.run(port=1235)
